d8df3e9c72411e8980606fa9907f69c080fa3fef
White.X <948169119@qq.com>
2018-06-21 18:08:36 +0800

    update

---
+++ Home/pages/view/orderOwner/addConstruinfo/addConstruinfo.js
@@ -0,0 +1,74 @@
+// pages/view/orderOwner/addConstruinfo/addConstruinfo.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 装修方式
+    typelist_renovation:['方式一',"方式二","方式三"],
+    renovationIndex:0,
+    // 计价方式
+    typelist_price:["方式一","方式二","方式三"],
+    priceIndex:0,
+    // 工种
+    typelist_worker: ["工种一", "工种二", "工种三"],
+    workerIndex: 0
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
